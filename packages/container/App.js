75199d82f4260967a688bc3a06a7e3123f2d0e85
vinnu239 <vinod.shanmukha000@gmail.com>
2023-11-29 14:33:16 +0530

    Intial commit

---
+++ packages/container/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MarketingApp from "./src/components/MarketingApp";
+export default () => {
+  return (
+
+      <>
+  <h1>Welcome Back</h1>
+  <MarketingApp/>
+  </>
+      );
+};
---
+++ packages/container/src/bootstarp.js
@@ -0,0 +1,6 @@
+// here mount function is not needed because the container is act has based and it take cares loading of components
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));

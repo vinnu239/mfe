071156de7834c989f368e2564a2158c60363558b
vinnu239 <vinod.shanmukha000@gmail.com>
2023-11-29 21:29:45 +0530

    Pushing APP file changes

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require('../package.json');
 

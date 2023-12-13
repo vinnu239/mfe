437e6f47b145c91144dcb0f43c7b9012708a4b29
vinnu239 <vinod.shanmukha000@gmail.com>
2023-12-13 20:44:56 +0530

    Adding public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,18 +1,22 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: '[name].[contenthash].js',
+    filename: "[name].[contenthash].js", // this is format of name that will see in the source file in chrome appendednwith script tag
+    // this will be help once we run the cloudfron domain name it try to access the finename (main.0e678.js)
+    //but our S3 bucket floder satrts with container and latest than our build files where our main.js is located
+    // so that why we are using below param
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
